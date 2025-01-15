e0629e9115a39bdd3cdf45a187d7aa2696022c1d
Nikushaana <nikanashvili@gmail.com>
2025-01-15 16:51:46 +0400

    Updated new

--- src/components/axios/Axios.js
+++ src/components/axios/Axios.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 const axiosFront = axios.create({
-  baseURL: `
-  ${process.env.REACT_APP_API_URL}/`,
+  baseURL: `${process.env.REACT_APP_API_URL}/`,
 });
 
 axiosFront.interceptors.request.use((config) => {
-  let token = localStorage.getItem("ptbox");
-  config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
