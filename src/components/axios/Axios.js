cabb5008c17f245da309dd75d229cb878a2f09c6
Nikushaana <nikanashvili@gmail.com>
2025-01-15 16:35:18 +0400

    Updated new

--- src/components/axios/Axios.js
+++ src/components/axios/Axios.js
@@ -3,3 +3,9 @@ import axios from "axios";
 export const axiosFront = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/`,
 });
+
+axiosFront.interceptors.request.use((config) => {
+  const token = `${process.env.REACT_APP_TOKEN}`;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
